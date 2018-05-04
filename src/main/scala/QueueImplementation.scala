ec7484a58a773a7d3b445c615e6a447e9b813c87
Carlos Silva <carlossilva@cjr.org.br>
2018-05-04 17:30:57 -0300

    deque and queue implementation and tests ok

--- src/main/scala/QueueImplementation.scala
+++ src/main/scala/QueueImplementation.scala
@@ -1,18 +1,20 @@
 class QueueImplementation[A] extends Queue[A] {
     private var _elements: LinkedList[A] = new LinkedListImplementation[A]
-
+    private var number: A = _
     def size(): Int = _elements.size
 
     def enqueue(value: A): Unit = {
         _elements.insert(value, size)
     }
 
-    def dequeue(): Unit = {
+    def dequeue(): A = {
         if (size > 0) {
+            number = _elements.find(0).value
 		    _elements.remove(0)
         } else {
             throw InvalidMethod()
         }
+        number
     }
 
 }
