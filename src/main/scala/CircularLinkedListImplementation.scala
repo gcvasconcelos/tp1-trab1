6b82d634b0c5cfebc651c41d12268e047137b585
Carlos Silva <carlossilva@cjr.org.br>
2018-05-06 21:37:51 -0300

    cll based on dll implementation

--- src/main/scala/CircularLinkedListImplementation.scala
+++ src/main/scala/CircularLinkedListImplementation.scala
@@ -1,15 +1,14 @@
-class CircularLinkedListImplementation[A] extends LinkedListImplementation[A] {
-	class CllNode[A](val value: A, var next: CllNode[A], var prev: CllNode[A])
+class CircularLinkedListImplementation[A] extends DoubleLinkedListImplementation[A] {
     private var _elements: LinkedList[A] = new LinkedListImplementation[A]
 	
 	private var _size: Int = 0
-    private var _head: CllNode[A] = _
+    private var _head: DllNode[A] = _
 
     override def size(): Int = _size
     override def head(): A = _head.value
 
-    override def insert(value: A, pos: Int): Unit = {
-        var node = new CllNode[A](value, null, null)
+    def insert(value: A, pos: Int): Unit = {
+        var node = new DllNode[A](value, null, null)
         if (pos >= 0 && pos <= _size){
             if (_size == 0) { 
                 _head = node 
@@ -22,8 +21,8 @@ class CircularLinkedListImplementation[A] extends LinkedListImplementation[A] {
                     _head = node
                 }
                 else {
-                    var prevNode = find(pos-1)
-                	var nextNode = find(pos+1)
+                    var prevNode = search(pos-1)
+                	var nextNode = search(pos+1)
                     node.prev = prevNode
                     prevNode.next = node
                     node.next = nextNode
@@ -36,15 +35,15 @@ class CircularLinkedListImplementation[A] extends LinkedListImplementation[A] {
         }
     }
     
-    override def remove(pos: Int): Unit = {
+    def remove(pos: Int): Unit = {
         if (pos >= 0 && pos <= _size){
             if (pos == 0) {
             	_head.next.prev = _head.prev
                 _head.prev.next = _head.next
                 _head = _head.next
             } else {
-                var prevNode = find(pos-1)
-                var nextNode = find(pos+1)
+                var prevNode = search(pos-1)
+                var nextNode = search(pos+1)
                 prevNode.next = nextNode
                 nextNode.prev = prevNode
             }
@@ -54,7 +53,7 @@ class CircularLinkedListImplementation[A] extends LinkedListImplementation[A] {
         }
     }
 
-    override def find(pos: Int): CllNode[A] = {
+    def search(pos: Int): DllNode[A] = {
         var nodeTemp = _head
         if (pos >= 0 && pos <= _size) {
             for (i <- 0 until pos ) {
