f45cac5a594d790a62dbe9a5ff8e5d32a8895f18
Gabriel Vasconcelos <gcvasconcelos98@gmail.com>
2018-05-06 21:59:03 -0300

    Sorting refactored

--- src/main/scala/Sorting.scala
+++ src/main/scala/Sorting.scala
@@ -1,11 +1,11 @@
 /**
- * 
- *
  * @author gcvasconcelos
+ * 
+ * Group of sort methods given an Array
  */
 
 trait Sorting[A] {
-  def bubbleSort(array: Array[A]): Array[A]
-  def insertionSort(array: Array[A]): Array[A]
-  def selectionSort(array: Array[A]): Array[A]
+  def bubbleSort(array: Array[A]): Array[A] // performs bubble sort and returns ordered array
+  def insertionSort(array: Array[A]): Array[A] // performs insertion sort and returns ordered array
+  def selectionSort(array: Array[A]): Array[A] // performs selection sort and returns ordered array
 }
--- src/test/scala/TestSorting.scala
+++ src/test/scala/TestSorting.scala
@@ -12,16 +12,16 @@ class TestSorting extends FlatSpec with Matchers with GivenWhenThen with BeforeA
 	
 	before {
 		sort = new SortingImplementation[Int]
-        array = Array(5,3,1,2,4)
+    array = Array(5,3,1,2,4)
 	}
 
-    it should "use bubble sort to sort an array" in {
-        sort.bubbleSort(array) should be (Array(1,2,3,4,5))
+  it should "use bubble sort to sort an array" in {
+    sort.bubbleSort(array) should be (Array(1,2,3,4,5))
 	}
-    it should "use insertion sort to sort an array" in {
-        sort.insertionSort(array) should be (Array(1,2,3,4,5))
+  it should "use insertion sort to sort an array" in {
+    sort.insertionSort(array) should be (Array(1,2,3,4,5))
 	}
-    it should "use selection sort to sort an array" in {
-        sort.selectionSort(array) should be (Array(1,2,3,4,5))
+  it should "use selection sort to sort an array" in {
+    sort.selectionSort(array) should be (Array(1,2,3,4,5))
 	}
 }
